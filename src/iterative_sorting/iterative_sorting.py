aba13292da2c256d523de577154b7434b1ae279e
bundickm <bundickm@gmail.com>
2019-10-07 13:32:58 -0700

    completed sort assignment

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -1,27 +1,50 @@
+def insertion_sort(items):
+    # Split the list into sorted and unsorted
+    # For each element in unsorted...
+    counter = 0
+    for i in range(1, len(items)):
+        print(items)
+        # Insert that element into the correct place in sorted by:
+        # Store the element in a temp variable
+        temp = items[i]
+        # Shifting all larger sorted elements to the right by 1
+        j = i
+        while j > 0 and temp < items[j - 1]:
+            counter += 1
+            items[j] = items[j - 1]
+            j -= 1
+        # Insert the element after the first smaller elememnt
+        items[j] = temp
+        print(f"counter: {counter}")
+    return items
+
+
 # TO-DO: Complete the selection_sort() function below 
-def selection_sort( arr ):
-    # loop through n-1 elements
+def selection_sort(arr):
     for i in range(0, len(arr) - 1):
-        cur_index = i
-        smallest_index = cur_index
-        # TO-DO: find next smallest element
-        # (hint, can do in 3 loc) 
-             
+      smallest_index = i
 
-
-
-        # TO-DO: swap
-
-
-
-
+      for j in range(i, len(arr)):
+        if arr[j] < arr[smallest_index]:
+          smallest_index = j
+      
+      arr[i], arr[smallest_index] = arr[smallest_index], arr[i]
     return arr
 
 
-# TO-DO:  implement the Bubble Sort function below
-def bubble_sort( arr ):
+def bubble_sort(arr):
+  swapped = False
+  
+  for _ in arr:
+    for i in range(len(arr)-1):
+      if arr[i] > arr[i+1]:
+        arr[i], arr[i+1] = arr[i+1], arr[i]
+        swapped = True
 
-    return arr
+    if not swapped:
+      break
+      
+  return arr
 
 
 # STRETCH: implement the Count Sort function below
